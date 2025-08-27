0d02a771ba95b84e9a0462bbb87fc6b92df6debb
JohnnysGithubAccount <taifa2907@gmail.com>
2025-08-27 14:27:56 +0700

    Clean up

--- smarthome_agent/planning/planner.py
+++
@@ -1,7 +0,0 @@
-from core.memory import add_schedule
-from datetime import datetime
-
-def plan_home_arrival(time_str: str):
-    """Schedule home prep before arrival time."""
-    add_schedule("Prepare home", time_str)
-    return f"Home will be ready before {time_str}."
